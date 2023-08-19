c832aeb224614f170bbf8e22fdd105e46431c86d
alpaca <alpteo13@gmail.com>
2023-08-19 18:56:51 +0800

    Update Install_gf.sh

--- GoatScan-FYP-Team-12/GoatScan/InstallPackages/Install_gf.sh
+++ GoatScan-FYP-Team-12/GoatScan/InstallPackages/Install_gf.sh
@@ -1,12 +1,3 @@
-#!/bin/bash
-
-# Install Go
-echo "Installing Go..."
-GO_VERSION="1.20.4"  # Specify the desired Go version
-wget https://golang.org/dl/go${GO_VERSION}.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go${GO_VERSION}.linux-amd64.tar.gz
-rm go${GO_VERSION}.linux-amd64.tar.gz
-
 # Set Go environment variables (Execute whenever gf is used)
 echo "Setting Go environment variables..."
 export PATH=$PATH:/usr/local/go/bin
@@ -23,19 +14,13 @@ mkdir ~/.gf
 git clone https://github.com/1ndianl33t/Gf-Patterns.git # Upload our pattern to Github and change this
 sudo cp ~/Gf-Patterns/*.json ~/.gf #Change "Gf-Patterns accordingly as well"
 
+
 # Update shell configuration
 echo "Updating shell configuration..."
 echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
 echo 'export GOPATH=$HOME/go' >> ~/.bashrc
 echo 'export PATH=$PATH:$GOPATH/bin' >> ~/.bashrc
 echo 'source $HOME/.gf/gf-completion.bash' >> ~/.bashrc
-
 echo "Installation completed successfully!"
 echo "Please restart your shell or run 'source ~/.bashrc' to start using the gf tool."
 
-
-# 1) Make script executable:
-#   chmod +x install_gf.sh
-
-# 2) Run Script
-#   ./install_gf.sh
